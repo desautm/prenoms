4f4de1fa6cf46c5a6684ecbfe89a367790cb4106
Marc-Andre Desautels <marc-andre.desautels@cstjean.qc.ca>
2018-09-10 15:23:39 -0400

    Added example, URL and BugReports for possible CRAN submission.

--- R/data.R
+++ R/data.R
@@ -14,4 +14,27 @@
 #' @importFrom tibble tibble
 #' @source \url{https://www.donneesquebec.ca/recherche/fr/dataset/banque-de-prenoms-garcons}
 #' @source \url{https://www.donneesquebec.ca/recherche/fr/dataset/banque-de-prenoms-filles}
+#' @examples
+#' \dontrun{
+#'   library(dplyr)
+#'   library(ggplot2)
+#'   library(prenoms)
+#'
+#'   # Prenoms des membres de ma famille
+#'   famille <- prenoms %>%
+#'     filter(
+#'       prenom == "Marc-Andre" & sexe == "M" |
+#'       prenom == "Laurent" & sexe == "M" |
+#'       prenom == "Melanie" & sexe == "F" |
+#'       prenom == "Anna" & sexe == "F"
+#'     ) %>%
+#'     group_by(prenom, annee, sexe) %>%
+#'     summarise(n = sum(n)) %>%
+#'     arrange(annee)
+#'
+#'   # Representation ggplot
+#'   ggplot(data = famille, aes(x = annee, y = n, color = prenom))+
+#'     geom_line()+
+#'     scale_x_continuous( breaks = seq(1980, 2020, by = 5))
+#' }
 "prenoms"
