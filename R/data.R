64d7da645928742a84f1490a83675097c5daab18
Marc-Andre Desautels <marc-andre.desautels@cstjean.qc.ca>
2018-09-10 10:10:04 -0400

    Trying to make TRAVIS work.

--- R/data.R
+++ R/data.R
@@ -1,15 +1,15 @@
-#' Prenoms des garcons et filles du Quebec de 1980 a 2017
+#' Prenoms des bebes du Quebec entre 1980 et 2017
 #'
-#' Une base de donnees contenant les prenoms masculins et feminins au
-#' Quebec de 1980 a 2017.
+#' Une base de donnes contenant les prenoms des enfants du Quebec
+#' entre 1980 et 2017.
 #'
-#' @format Une base de donnees avec 13 511 308 lignes et 5 colonnes:
+#' @format Une base de donnees contenant 13 511 308 lignes et 5 colonnes:
 #' \describe{
 #'   \item{annee}{Annee}
 #'   \item{sexe}{F pour feminin et M pour masculin}
 #'   \item{prenom}{Prenom}
-#'   \item{n}{Frequence d apparition du prenom}
-#'   \item{prop}{Proportion du prenom}
+#'   \item{n}{Frequence}
+#'   \item{prop}{Proportion}
 #' }
 #' @source \url{https://www.donneesquebec.ca/recherche/fr/dataset/banque-de-prenoms-garcons}
 #' @source \url{https://www.donneesquebec.ca/recherche/fr/dataset/banque-de-prenoms-filles}
