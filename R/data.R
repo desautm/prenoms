8efa85f401cd8b5b98fd03564c8548ef9cf1830d
Marc-André Désautels <desautm@hotmail.com>
2021-04-30 09:26:45 -0400

    First release
    Add the .9000 suffix to the Version field in the DESCRIPTION to indicate that this is a development version.

--- R/data.R
+++ R/data.R
@@ -14,7 +14,6 @@
 #' @source \url{https://www.donneesquebec.ca/recherche/dataset/bec46ea8-7bd1-4d81-b9e0-ea9f3ba0c59d/resource/fe6aea67-c2de-42f9-a21b-2db1b35e2f5f/download/gars1980-2020.csv}
 #' @source \url{https://www.donneesquebec.ca/recherche/dataset/13db2583-427a-4e5f-b679-8532d3df571f/resource/e1f20072-935d-4a92-91c4-61a12fbe687b/download/filles1980-2020.csv}
 #' @examples
-#' \dontrun{
 #'   library(dplyr)
 #'   library(ggplot2)
 #'   library(prenoms)
@@ -34,9 +33,7 @@
 #'   ggplot(data = family, aes(x = year, y = n, color = name))+
 #'   geom_line()+
 #'   scale_x_continuous( breaks = seq(1980, 2020, by = 5))
-#' }
 #'
-#' \dontrun{
 #'   library(dplyr)
 #'
 #'   # Les 5 prenoms feminins les plus populaires en 2020
@@ -45,5 +42,4 @@
 #'   select(year, sex, name, n) %>%
 #'   arrange(desc(n)) %>%
 #'   head(5)
-#' }
 "prenoms"
--- cran-comments.md
+++ cran-comments.md
@@ -6,5 +6,6 @@
 There were no ERRORs or WARNINGs or NOTEs. 
 
 ## Fix for resubmit
-* new version number from 0.0.9000 to 0.0.1
-* got rid of `This library contains`
+* Changed links to the used webservices to the description field of your DESCRIPTION file in the form <http:...> or <https:...> with angle brackets for auto-linking and no space after 'http:' and 'https:'. I don't know if I did the right thing, I added angle brackets in the URL field.
+* The examples take less than 5 seconds to compile so I got rid of \dontrun{}
+* Added Suggests field in DESCRIPTION to run the examples.
